f8770f49a411dc451f097fe9c2e3ab084a551e52
Hardik Sachan <sachanhardik@gmail.com>
2024-07-05 05:37:05 +0530

    pass stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,8 +7,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let _ = stream.write(b"+PONG\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
