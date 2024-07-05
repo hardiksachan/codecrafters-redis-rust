904566cab30890caf37ec654a180457294916cc7
Hardik Sachan <sachanhardik@gmail.com>
2024-07-05 09:37:13 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
 };
 
-fn main() {
-    println!("Logs from your program will appear here!");
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            process(stream).await;
+        });
+    }
+}
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buf = [0; 512];
-                loop {
-                    let n = stream.read(&mut buf).unwrap();
-                    if n == 0 {
-                        break;
-                    }
-
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+async fn process(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    loop {
+        let n = stream.read(&mut buf).await.unwrap();
+        if n == 0 {
+            break;
         }
+
+        stream.write(b"+PONG\r\n").await.unwrap();
     }
 }
